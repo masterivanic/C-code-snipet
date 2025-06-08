43d1341d2e0fb43e498b6393051125412ebdbafa
masterivanic <masterivanic@gmail.com>
2025-06-08 03:32:30 +0200

    Add struc and union data structure

--- program.c
+++ program.c
@@ -6,6 +6,26 @@
  * Storage Class Specifiers
     Define the storage duration of an object.
  */
+
+ // Structs are user defined data storage objects containing public members by default.
+ struct my_struct {
+    int x;
+    int y;
+};
+
+/**
+ * A union is a special data type that can store different data types at the same memory location. 
+ * You can define a union with many members, but only one member can contain a value at any given time. 
+ * Unions provide an efficient way of using the same memory location for multiple purposes. 
+ * A union’s size will be the size of the largest constituent type.
+ */
+
+union my_data {
+    int i;
+    float f;
+    char str[20];
+};
+
 int main(int argc, char *argv[])
 {
     extern int a;               // defined elsewhere
@@ -21,6 +41,26 @@ int main(int argc, char *argv[])
     {
         printf("%s\n", argv[i]);
     }
+
+    /**
+     * Perform integer addition or subtraction operations on pointers, taking the data type’s size
+     * into account to return the correct address of the next item.
+     */
+    int x[5];
+    int* x_ptr = &x[0];
+    printf("Value of x_ptr = %p\n", (void*)x_ptr);
+    printf("Value of x_ptr + 1 = %p\n", (void*)(x_ptr + 1));
+    char m[5];
+    char* y_ptr = &m[0];
+    printf("Value of y_ptr = %p\n", (void*)y_ptr);
+    printf("Value of y_ptr + 1 = %p\n", (void*)(y_ptr + 1));
+
+    struct my_struct object1;
+    object1.x = 1;
+    printf("%d\n", object1.x);
+
+    union my_data object2;        
+    printf("Size of my_data union: %lu\n", sizeof(object2));
 
     return 0;
 }
